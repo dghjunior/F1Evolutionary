561554d6201299b88770e78c6e29c2581415868b
dghjunior <dghjunior@gmail.com>
2022-11-11 18:39:10 -0500

    Updated to match framework code

--- f1_ga.py
+++ f1_ga.py
@@ -62,10 +62,18 @@ final_gear_red = 7
 #ninth_ratio
 #tenth_ratio
 
+def evalLapTime():
+    return 0.0
 
-#TODO
+def cxIntermediate(ratio):
+    return 0.0
+
+def mutationAdaptFeasible():
+    return 0.0
+
+
 creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
-creator.create("Individual", array.array, typecode='d', fitness=creator.FitnessMin)
+creator.create("Individual", list, fitness=creator.FitnessMin)
 
 toolbox = base.Toolbox()
 
@@ -75,35 +83,26 @@ toolbox.register("attr_float", random.uniform, -5, 5)
 toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.attr_float, 51)
 toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 
-def checkBounds(min, max):
-    def decorator(func):
-        def wrappper(*args, **kargs):
-            offspring = func(*args, **kargs)
-            for child in offspring:
-                for i in range(len(child)):
-                    if child[i] > max:
-                        child[i] = max
-                    elif child[i] < min:
-                        child[i] = min
-            return offspring
-        return wrappper
-    return decorator
-
-toolbox.register("evaluate", benchmarks.kursawe)
-toolbox.register("mate", tools.cxBlend, alpha=1.5)
-toolbox.register("mutate", tools.mutGaussian, mu=0, sigma=3, indpb=0.3)
-toolbox.register("select", tools.selNSGA2)
-
-toolbox.decorate("mate", checkBounds(-5, 5))
-toolbox.decorate("mutate", checkBounds(-5, 5))
+toolbox.register("evaluate", evalLapTime)
+toolbox.register("mate", cxIntermediate, ratio = 0.8)
+toolbox.register("mutate", mutationAdaptFeasible)
+toolbox.register("select", tools.selTournament, tournsize=2)
 
 def main():
-    random.seed(64)
+    random.seed()
 
+    pop = toolbox.population(n=50)
+    hof = tools.HallOfFame(3)
     stats = tools.Statistics(lambda ind: ind.fitness.values)
-    stats.register("avg", numpy.mean, axis=0)
-    stats.register("std", numpy.std, axis=0)
-    stats.register("min", numpy.min, axis=0)
-    stats.register("max", numpy.max, axis=0)
+    stats.register("Avg", numpy.mean)
+    stats.register("Std", numpy.std)
+    stats.register("Min", numpy.min)
+    stats.register("Max", numpy.max)
 
-    return stats
+    algorithms.eaSimple(pop, toolbox, cxpb=1, mutpb=0.5, ngen=500, stats=stats, halloffame=hof, verbose=True)
+
+    return pop, stats, hof
+
+
+if __name__ == "__main__":
+    main()
