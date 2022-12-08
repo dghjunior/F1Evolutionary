14db576f1290e717fe086d653bbc81afc6b061d0
Daniel Harper <dghjunior@gmail.com>
2022-12-07 23:52:22 -0500

    Added simluated binary crossover

--- f1_ga.py
+++ f1_ga.py
@@ -274,10 +274,11 @@ toolbox.register("individual", tools.initCycle, creator.Individual, (toolbox.veh
 )
 toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 toolbox.register("evaluate", evalLapTime)
-toolbox.register("mate", cxIntermediate, ratio=0.8)
+#toolbox.register("mate", cxIntermediate, ratio=0.8)
 #toolbox.register("mate", tools.cxUniform, indpb=0.8)
+toolbox.register("mate", tools.cxSimulatedBinaryBounded, eta=0.5, low=map(lambda x: x[0], bounds), up=map(lambda x: x[1], bounds))
 toolbox.register("mutate", mutationpower, indpb=0.3)
-toolbox.register("select", tools.selTournament, tournsize=3)
+toolbox.register("select", tools.selTournament, tournsize=2)
 
 def main():
     shutil.rmtree('Individuals')
@@ -293,7 +294,7 @@ def main():
     stats.register("Min", numpy.min)
     stats.register("Max", numpy.max)
 
-    algorithms.eaSimple(pop, toolbox, cxpb=0.9, mutpb=0.4, ngen=100, stats=stats, halloffame=hof, verbose=True)
+    algorithms.eaSimple(pop, toolbox, cxpb=0.8, mutpb=0.5, ngen=100, stats=stats, halloffame=hof, verbose=True)
 
     print(hof[0])
 
